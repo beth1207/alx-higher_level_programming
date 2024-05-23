ee97cabd47c0285511567f72856d4838cdefe29e
ankunda <ankundabeth07@gmail.com>
2024-05-23 02:11:14 -0700

    16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,2 @@
-SELECT score, name
-FROM second_table
-HAVING name IS NOT NULL
-ORDER BY score DESC;
+-- lista all records in a table except those wil no name value
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
